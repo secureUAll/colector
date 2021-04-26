fa0c79fda3d5f2da241ea52a1bc67eed6dab4a72
Guida <margaridasmartins00@gmail.com>
2021-04-26 20:46:21 +0100

    small fix

--- colector/colector.py
+++ colector/colector.py
@@ -8,10 +8,8 @@ import time
 #mongo_client = MongoClient("mongodb://localhost:27017/")
 #print(mongo_client)
 
-
-
 time.sleep(10)
-
+logging.warning("Colector started")
 
 producer = KafkaProducer(bootstrap_servers='kafka:9092',
                           security_protocol='SSL',
@@ -21,10 +19,6 @@ producer = KafkaProducer(bootstrap_servers='kafka:9092',
                           ssl_check_hostname=False,
                           api_version=(2,7,0))
 
-# Write hello world to test topic
-p=producer.send('test', b'Hello World')
-producer.flush()
-print(p)
 consumer = KafkaConsumer('test',bootstrap_servers='kafka:9092',
                           security_protocol='SSL',
                           ssl_cafile='./colector_certs/CARoot.pem',
@@ -32,6 +26,12 @@ consumer = KafkaConsumer('test',bootstrap_servers='kafka:9092',
                           ssl_keyfile='./colector_certs/key.pem',
                           ssl_check_hostname=False,
                           api_version=(2,7,0))
+
+# Write hello world to test topic
+producer.send('test', b'Hello World')
+producer.flush()
+logging.warning(consumer.topics())
+
 for msg in consumer:
-    print("AAAAAAAAAAAAAAAAAAAAAAAA" +msg)
-    break
+    logging.warning("Message"+ str(msg))
+    time.sleep(3)
