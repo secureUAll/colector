e0c3971f2b1ea8732b616a290fc96ca4de5a1bda
Guida <margaridasmartins00@gmail.com>
2021-05-01 14:59:29 +0100

    initial commit not working

--- colector/colector.py
+++ colector/colector.py
@@ -3,15 +3,43 @@ from kafka import KafkaConsumer, KafkaProducer
 from kafka.errors import KafkaError
 import logging
 import time
+import json
 
-
-#mongo_client = MongoClient("mongodb://localhost:27017/")
-#print(mongo_client)
+#logging.basicConfig(level=logging.DEBUG)
 
 time.sleep(10)
 logging.warning("Colector started")
+#mongo_client = MongoClient("mongodb://localhost:27017/")
+#print(mongo_client)
 
-producer = KafkaProducer(bootstrap_servers='kafka:9092',
+
+def main():
+ 
+    logging.warning(consumer.subscription())
+
+    for msg in consumer:
+        
+        if msg.topic == colector_topics[0]:
+            logging.warning("Received a init message")
+
+            #guard configuration on BD and retrive id
+
+            #send id
+            producer.send(colector_topics[0],key=msg.key, value={'STATUS':'200','WORKER_ID':'1'})
+
+        elif msg.topic == colector_topics[1]:
+            logging.warning("Received a scan request message")
+        else:
+            logging.warning("Message topic: "+ msg.topic + " does not exist" )
+        time.sleep(3)
+
+if __name__ == "__main__":
+
+    #topics colector subscribes 
+    colector_topics=['INIT','SCAN_REQUEST']
+
+    #kafka producer
+    producer = KafkaProducer(bootstrap_servers='kafka:9092',
                           security_protocol='SASL_SSL',
                           ssl_cafile='./colector_certs/CARoot.pem',
                           ssl_certfile='./colector_certs/certificate.pem',
@@ -20,9 +48,11 @@ producer = KafkaProducer(bootstrap_servers='kafka:9092',
                           sasl_plain_username='colector',
                           sasl_plain_password='colector',
                           ssl_check_hostname=False,
-                          api_version=(2,7,0))
+                          api_version=(2,7,0),
+                          value_serializer=lambda m: json.dumps(m).encode('latin'))
 
-consumer = KafkaConsumer('test',bootstrap_servers='kafka:9092',
+    #kafka consumer
+    consumer = KafkaConsumer(bootstrap_servers='kafka:9092',
                           security_protocol='SASL_SSL',
                           ssl_cafile='./colector_certs/CARoot.pem',
                           ssl_certfile='./colector_certs/certificate.pem',
@@ -31,13 +61,7 @@ consumer = KafkaConsumer('test',bootstrap_servers='kafka:9092',
                           sasl_plain_username='colector',
                           sasl_plain_password='colector',
                           ssl_check_hostname=False,
-                          api_version=(2,7,0))
-
-# Write hello world to test topic
-producer.send('test', b'Hello World')
-producer.flush()
-logging.warning(consumer.topics())
-
-for msg in consumer:
-    logging.warning("Message"+ str(msg))
-    time.sleep(3)
+                          api_version=(2,7,0),
+                          value_deserializer=lambda m: json.dumps(m).encode('latin'))
+    consumer.subscribe(colector_topics)
+    main()
---
+++ colector/dumb_worker.py
@@ -0,0 +1,49 @@
+from pymongo import MongoClient
+from kafka import KafkaConsumer, KafkaProducer
+from kafka.errors import KafkaError
+import logging
+import os 
+import json
+import time
+#logging.basicConfig(level=logging.DEBUG)
+time.sleep(30)
+colector_topics=['INIT','SCAN_REQUEST']
+
+#kafka producer
+producer = KafkaProducer(bootstrap_servers='kafka:9092',
+                          security_protocol='SASL_SSL',
+                          ssl_cafile='./colector_certs/CARoot.pem',
+                          ssl_certfile='./colector_certs/certificate.pem',
+                          ssl_keyfile='./colector_certs/key.pem',
+                          sasl_mechanism='PLAIN',
+                          sasl_plain_username='worker',
+                          sasl_plain_password='worker',
+                          ssl_check_hostname=False,
+                          api_version=(2,7,0),
+                          value_serializer=lambda m: json.dumps(m).encode('latin'))
+
+#kafka consumer
+consumer = KafkaConsumer(bootstrap_servers='kafka:9092',
+                          security_protocol='SASL_SSL',
+                          ssl_cafile='./colector_certs/CARoot.pem',
+                          ssl_certfile='./colector_certs/certificate.pem',
+                          ssl_keyfile='./colector_certs/key.pem',
+                          sasl_mechanism='PLAIN',
+                          sasl_plain_username='worker',
+                          sasl_plain_password='worker',
+                          ssl_check_hostname=False,
+                          api_version=(2,7,0),
+                          value_deserializer=lambda m: json.dumps(m).decode('latin'))
+consumer.subscribe(colector_topics)
+
+#init message 
+random= os.urandom(16)
+message={'CONFIG':{'ADDRESS_LIST':['123.4.4.4','123.3.3.3']}}
+
+producer.send(colector_topics[0], key=random , value=message)
+producer.flush()
+
+for message in consumer:
+    print(message.topic)
+    print(message.key)
+    
