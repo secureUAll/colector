155560ce9ea8efcdcdfc19fc3366e854a6e676b1
margaridasmartins <margarida.martins@ua.pt>
2021-05-09 23:21:58 +0100

    see what worker returns

--- colector/script.sh
+++ colector/script.sh
@@ -5,4 +5,4 @@ exec celery -A celery_worker worker -l info &
 exec celery  -A celery_worker beat -l info &
 exec python3 colector.py &
 #exec python3 worker.py 
-exec python3 dumb_worker.py
+#exec python3 dumb_worker.py
