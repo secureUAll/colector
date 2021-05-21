e8583bb1053d3006fce7131db275de388b091f55
moraisandreua <48691769+moraisandreua@users.noreply.github.com>
2021-05-21 18:59:18 +0100

    teste
    - testing tests

--- colector/tests/test_colector.py
+++ colector/tests/test_colector.py
@@ -1,4 +1,5 @@
 from colector_main import Main
+from Heartbeat import Heartbeat
 from unittest.mock import patch
 from unittest.mock import MagicMock
 
@@ -8,3 +9,8 @@ def test_initialworker():
     Main.producer = MagicMock()
     Main.initial_worker(Main,{"CONFIG":{"ADDRESS_LIST":["2.2.2"]}},bytes([2]))
 
+def test_heartbeat():
+    hb=Heartbeat()
+    hb.startup()
+    print(hb.workers)
+    pass
