efef7ef6b0a8fd240798e6ad844604758ec95c69
margaridasmartins <margarida.martins@ua.pt>
2021-06-02 15:48:24 +0100

    comparing tools general results

--- colector/report.py
+++ colector/report.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 #TODO
 """
 Disable Scan in some ports
@@ -23,8 +25,10 @@ class Report():
         self.msg= msg
         self.cur= self.conn.cursor()
 
-        self.initialize_ids()
-        self.save_general_info()
+        success_scan =self.initialize_ids()
+        if success_scan:
+            self.save_general_info()
+            self.save_vulnerabilities_info()
         self.cur.close()
 
 
@@ -32,39 +36,77 @@ class Report():
         self.cur.execute(self.QUERY_MACHINE,(self.msg.value["MACHINE"],self.msg.value["MACHINE"]))
         self.machine_id= self.cur.fetchone()[0]
 
-        self.cur.execute(self.QUERY_SAVE_SCAN,("2",self.machine_id ,int.from_bytes(self.msg.key,"big")))
+        status= self.check_machine_satus()
+        self.cur.execute(self.QUERY_SAVE_SCAN,(status,self.machine_id ,int.from_bytes(self.msg.key,"big")))
         self.scan_id= self.cur.fetchone()[0]
         self.conn.commit()
 
+        if status=="UP":
+            return True
 
-    def save_general_info(self):
+        return False
+
+    def check_machine_status(self):
+        status= "DOWN"
+
         result_scan=self.msg.value["RESULTS"]
+        for tool in result_scan:
+            if tool['TOOL']=="nikto":
+                if 'status' not in tool:
+                    status="UP"
+            elif tool['TOOL']=="nmap":
+                 if tool['host']['up']=='1':
+                    status="UP"
+            elif tool['TOOL']=="vulscan":
+                if tool['state']=='up':
+                    status="UP"
+        return status
+
+    def save_vulnerabilities_info(self):
+        pass
+
+    def save_general_info(self):     
+        tools_general_data= self.get_tools_general_data()        
+
+        address_ip=Counter(tools_general_data["address_ip"]).most_common(1)[0][0]
+        address_dns=Counter(tools_general_data["address_name"]).most_common(1)[0][0]
+        self.cur.execute(self.QUERY_UPDATE_ADDRESS,(address_ip,address_dns,self.machine_id))
+        self.conn.commit()
+
+        for k in tools_general_data:
+            if k!= "address_ip" or k!= "address_name":
+                service_name=Counter(k[service_name]).most_common(1)[0][0]
+                service_version=Counter(k[service_version]).most_common(1)[0][0]
+                self.cur.execute(self.QUERY_MACHINE_SERVICE, (service_name,service_version))
+                service_id= self.cur.fetchone()[0]
+                self.conn.commit()
+                self.cur.execute(self.QUERY_MACHINE_PORT, (int(k),self.machine_id,service_id))
+                self.conn.commit()      
+
         
+
+    def get_tools_general_data(self):
+        result_scan=self.msg.value["RESULTS"]
+        tools_general_data= {"address_ip":[],"address_name":[]}
+
         for tool in result_scan:
             if 'address' in tool:
                 logging.warning("adding ip: " + tool["address"]["address_ip"] + "adding dns: " + tool["address"]["address_name"] )
-                address_ip= tool["address"]["address_ip"]
-                address_dns= tool["address"]["address_name"]
-                self.cur.execute(self.QUERY_UPDATE_ADDRESS,(address_ip,address_dns,self.machine_id))
-                self.conn.commit()
+                tools_general_data["address_ip"].append(tool["address"]["address_ip"])
+                tools_general_data["address_name"].append(tool["address"]["address_name"])
 
             if 'ports' in tool:
                 logging.warning("adding ports")
                 ports= tool["ports"]
                 for p in ports:
-                    self.save_port(p,self.cur)
-        
-
-    def save_port(self,port,cur):
-        port_id = port["id"]
-        service_name= port["name"]
-        service_version= port["product"] + port["version"]
+                    port_id = str(p["id"])
+                    service_name= p["name"]
+                    service_version= p["product"] + p["version"]
+                    if port_id not in tools_general_data:
+                        tools_general_data[port_id]={"service_name":[], "service_version":[]}
+                    tools_general_data[port_id]["service_name"].append(service_name)
+                    tools_general_data[port_id]["service_version"].append(service_version)
+                    
 
         logging.warning("port id: " + str(port_id) + " service name: " + service_name + " service version: " +service_version )
-
-        self.cur.execute(self.QUERY_MACHINE_SERVICE, (service_name,service_version))
-        service_id= cur.fetchone()[0]
-        self.conn.commit()
-        self.cur.execute(self.QUERY_MACHINE_PORT, (port_id,self.machine_id,service_id))
-        self.conn.commit()
-
+        return tools_general_data
