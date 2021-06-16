f250572c35e99ca513b951a6c8ddd9adc41a8e8a
margaridasmartins <margarida.martins@ua.pt>
2021-06-16 03:27:05 +0100

    setting teams

--- colector/notification_sender.py
+++ colector/notification_sender.py
@@ -14,7 +14,7 @@ class NotificationSender():
     def run(self):
         logging.warning("Notifications for machine" + str(self.info["MACHINE_ID"]))
 
-        QUERY_USER_EMAILS= "select lu.first_name, ln.type, ln.value  from  machines_machine mm, machines_machineuser mu, login_user lu, login_usernotification ln where mm.id=%s AND mm.id=mu.machine_id  AND mu.user_id=lu.id AND ln.id=lu.id"
+        QUERY_USER_EMAILS= "select lu.first_name, ln.type, ln.value  from  machines_machine mm, machines_machineuser mu, login_user lu, login_usernotification ln where mm.id=%s AND mm.id=mu.machine_id  AND mu.user_id=lu.id AND ln.user_id=lu.id"
         
         QUERY_MACHINE = "select ip, dns, \"scanLevel\", risk from machines_machine where id=%s"
         QUERY_SCAN= "select date from  machines_scan where machine_id=%s  ORDER BY date  DESC LIMIT 1"
@@ -43,7 +43,7 @@ class NotificationSender():
         host= data[1] if data[1] != '' else data[0]
 
         for u in user_info:
-            notify= EmailNotify() if user_info[1]=="Email" else TeamsNotify()
+            notify= EmailNotify() if u[1]=="Email" else TeamsNotify()
             if "NVULNS" in self.info:
                 if(self.info["NVULNS"]==0):
                     Templates.hostup_novulns(notify, u, host, scan_date , self.info["MACHINE_ID"] ,data[2])
