aa1edb14c4d70062fb4eab1288856e44a55aad38
margaridasmartins <margarida.martins@ua.pt>
2021-06-20 13:26:52 +0100

    experimenting

--- colector/colector.py
+++ colector/colector.py
@@ -10,11 +10,22 @@ logging.basicConfig(level=logging.DEBUG)
 #docker exec -it docker_db_1 bash
 #psql -h 127.0.0.1 -d secureuall -U frontend
 
+
+def start_celery():
+    try:
+        ready=celery_worker.main.delay()
+
+        #stay up
+        while True:
+            time.sleep(30)
+
+    except:
+        start_celery()
+    
+
 if __name__ == "__main__":
-    ready=celery_worker.main.delay()
-    #stay up
-    while True:
-        time.sleep(30)
+    start_celery()
+    
 
 
 
